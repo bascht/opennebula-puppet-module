8179acadb275afa8d8f97da66d6bdbe68a5dc6b8
Sebastian Schulze <github.com@bascht.com>
2014-08-27 08:17:49 +0200

    Use dynamic properties for onetemplate mapping
    * Extract login + invocation into separate module
    * Assign properties dynamically
    * Test for correct invocation & stub out executables
    * Extract ERB templates from puppet providers
    * Pull out ERBs into a separate module and just include them from there
    * Add a proper XML file for testing
    * Clarify variable names

--- lib/puppet/provider/onetemplate/onetemplate.rb
+++ lib/puppet/provider/onetemplate/onetemplate.rb
@@ -1,86 +1,60 @@
 require 'rexml/document'
 require 'tempfile'
 require 'erb'
+require 'puppet/util/opennebula'
 
 Puppet::Type.type(:onetemplate).provide(:onetemplate) do
   desc "onetemplate provider"
+
+  include Puppet::Util::Opennebula::CLI
+  extend Puppet::Util::Opennebula::Properties
 
   commands :onetemplate => "onetemplate"
 
   mk_resource_methods
 
+  property_map :cpu     => "VMTEMPLATE/TEMPLATE/CPU",
+    :memory             => "VMTEMPLATE/TEMPLATE/MEMORY",
+    :vcpu               => "VMTEMPLATE/TEMPLATE/VCPU",
+    :os_kernel          => "VMTEMPLATE/TEMPLATE/OS/KERNEL",
+    :os_initrd          => "VMTEMPLATE/TEMPLATE/OS/INITRD",
+    :os_arch            => "VMTEMPLATE/TEMPLATE/OS/ARCH",
+    :os_root            => "VMTEMPLATE/TEMPLATE/OS/ROOT",
+    :os_kernel_cmd      => "VMTEMPLATE/TEMPLATE/OS/KERNELCMD",
+    :os_bootloader      => "VMTEMPLATE/TEMPLATE/OS/BOOTLOADER",
+    :os_boot            => "VMTEMPLATE/TEMPLATE/OS/BOOT",
+    :acpi               => "VMTEMPLATE/TEMPLATE/FEATURES/ACPI",
+    :pae                => "VMTEMPLATE/TEMPLATE/FEATURES/PAE",
+    :pci_bridge         => "VMTEMPLATE/TEMPLATE/FEATURES/PCI_BRIDGE",
+    :disks              => "VMTEMPLATE/TEMPLATE/DISK/IMAGE",
+    :nics               => "VMTEMPLATE/TEMPLATE/NIC/NETWORK",
+    :nic_model          => "VMTEMPLATE/TEMPLATE/NIC/MODEL",
+    :graphics_type      => "VMTEMPLATE/TEMPLATE/GRAPHICS/TYPE",
+    :graphics_listen    => "VMTEMPLATE/TEMPLATE/GRAPHICS/LISTEN",
+    :graphics_port      => "VMTEMPLATE/TEMPLATE/GRAPHICS/PORT",
+    :graphics_passwd    => "VMTEMPLATE/TEMPLATE/GRAPHICS/PASSWORD",
+    :graphics_keymap    => "VMTEMPLATE/TEMPLATE/GRAPHICS/KEYMAP",
+    :context_ssh        => "VMTEMPLATE/TEMPLATE/CONTEXT/SSH",
+    :context_ssh_pubkey => "VMTEMPLATE/TEMPLATE/CONTEXT/SSH_PUBLIC_KEY",
+    :context_network    => "VMTEMPLATE/TEMPLATE/CONTEXT/NETWORK",
+    :context_onegate    => "VMTEMPLATE/TEMPLATE/CONTEXT/ONEGATE",
+    :context_files      => "VMTEMPLATE/TEMPLATE/CONTEXT/FILES_DS"
+
   # Create a VM template with onetemplate by passing in a temporary template definition file.
   def create
+    content = Puppet::Util::Opennebula::Templates.onetemplate.result(binding)
     file = Tempfile.new("onetemplate-#{resource[:name]}")
-    template = ERB.new <<-EOF
-<TEMPLATE>
-  <NAME><%=   resource[:name]   %></NAME>
-  <MEMORY><%= resource[:memory] %></MEMORY>
-  <CPU><%=    resource[:cpu]    %></CPU>
-  <VCPU><%=   resource[:vcpu]   %></VCPU>
-
-  <OS>
-<% if resource[:os_kernel]     %>    <KERNEL><%=     resource[:os_kernel]     %></KERNEL><% end %>
-<% if resource[:os_initrd]     %>    <INITRD><%=     resource[:os_initrd]     %></INITRD><% end %>
-<% if resource[:os_arch]       %>    <ARCH><%=       resource[:os_arch]       %></ARCH><% end %>
-<% if resource[:os_root]       %>    <ROOT><%=       resource[:os_root]       %></ROOT><% end %>
-<% if resource[:os_kernel_cmd] %>    <KERNEL_CMD><%= resource[:os_kernel_cmd] %></KERNEL_CMD><% end %>
-<% if resource[:os_bootloader] %>    <BOOTLOADER><%= resource[:os_bootloader] %></BOOTLOADER><% end %>
-<% if resource[:os_boot]       %>    <BOOT><%=       resource[:os_boot]       %></BOOT><% end %>
-  </OS>
-
-  <DISK>
-<% resource[:disks].each do |disk| %>
-    <IMAGE><%= disk %></IMAGE>
-<% end %>
-  </DISK>
-<% resource[:nics].each do |nic| %>
-  <NIC>
-    <% if resource[:nic_model] %><MODEL><%= resource[:nic_model] %></MODEL><% end %>
-    <NETWORK><%= nic %></NETWORK>
-  </NIC>
-<% end %>
-
-  <GRAPHICS>
-<% if resource[:graphics_type]   %><TYPE><%=   resource[:graphics_type]   %></TYPE><% end %>
-<% if resource[:graphics_listen] %><LISTEN><%= resource[:graphics_listen] %></LISTEN><% end %>
-<% if resource[:graphics_port]   %><PORT><%=   resource[:graphics_port]   %></PORT><% end %>
-<% if resource[:graphics_passwd] %><PASSWD><%= resource[:graphics_passwd] %></PASSWD><% end %>
-<% if resource[:graphics_keymap] %><KEYMAP><%= resource[:graphics_keymap] %></KEYMAP><% end %>
-  </GRAPHICS>
-  <FEATURES>
-<% if resource[:acpi] %><ACPI><%= resource[:acpi] %></ACPI><% end %>
-<% if resource[:pae]  %><PAE><%=  resource[:pae]  %></PAE><%  end %>
-  </FEATURES>
-  <CONTEXT>
-<% if resource[:context_network] %><NETWORK><%= resource[:context_network] %></NETWORK><% end %>
-<% if resource[:context_files] %>
-    <FILES_DS><% resource[:context_files].each do |context_file| %>$FILE[IMAGE="<%= context_file %>"] <% end %></FILES_DS>
-<% end %>
-<% if resource[:context] %>
-<% resource[:context].each do |key, value| %>
-   <<%= key %>><%= value %></<%= key %>>
-<% end %>
-<% end %>
-<% if resource[:context_ssh_pubkey] %><SSH_PUBLIC_KEY><%= resource[:context_ssh_pubkey] %></SSH_PUBLIC_KEY><% end %>
-  </CONTEXT>
-</TEMPLATE>
-EOF
-
-    tempfile = template.result(binding)
-    file.write(tempfile)
+    file.write content
     file.close
-    self.debug "Creating template using #{tempfile}"
-    output = "onetemplate create #{file.path} ", self.class.login
-    `#{output}`
+    self.debug "Creating template using #{content}"
+    self.invoke 'create', file.path
     file.delete
     @property_hash[:ensure] = :present
   end
 
   # Destroy a VM using onevm delete
   def destroy
-    output = "onetemplate delete #{resource[:name]} ", self.class.login
-    `#{output}`
+    self.invoke 'delete', resource[:name]
     @property_hash.clear
   end
 
@@ -91,8 +65,8 @@ EOF
 
   # Return the full hash of all existing onevm resources
   def self.instances
-    output = "onetemplate list -x ", login
-    REXML::Document.new(`#{output}`).elements.collect("VMTEMPLATE_POOL/VMTEMPLATE") do |template|
+    output = self.invoke 'list', '--xml'
+    REXML::Document.new(output).elements.collect("VMTEMPLATE_POOL/VMTEMPLATE") do |template|
       elements = template.elements
       new(
         :name                      => elements["NAME"].text,
@@ -139,109 +113,5 @@ EOF
         resources[name].provider = provider
       end
     end
-  end
-
-  # login credentials
-  def self.login
-    credentials = File.read('/var/lib/one/.one/one_auth').strip.split(':')
-    user = credentials[0]
-    password = credentials[1]
-    login = " --user #{user} --password #{password}"
-    login
-  end
-
-  # setters
-  def memory=(value)
-      raise "Can not yet modify memory on a template"
-  end
-  def cpu=(value)
-      raise "Can not yet modify cpu on a template"
-  end
-  def vcpu=(value)
-      raise "Can not yet modify vcpu on a template"
-  end
-  def os_kernel=(value)
-      raise "Can not modify kernel on a template"
-  end
-  def os_initrd=(value)
-      raise "Can not modify initrd on a template"
-  end
-  def os_arch=(value)
-      raise "Can not modify arch on a template"
-  end
-  def os_root=(value)
-      raise "Can not modify root device on a template"
-  end
-  def os_kernel_cmd=(value)
-      raise "Can not modify kernel cmd on a template"
-  end
-  def os_bootloader=(value)
-      raise "Can not modify booloader options on a template"
-  end
-  def os_boot=(value)
-      raise "Can not modify boot device on a template"
-  end
-  def acpi=(value)
-      raise "Can not modify acpi on a template"
-  end
-  def pae=(value)
-      raise "Can not modify pae on a template"
-  end
-  def pci_bridge=(value)
-      raise "Can not modify pci_bridge on a template"
-  end
-  def disks=(value)
-      raise "Can not yet modify disks on a template"
-  end
-  def nics=(value)
-      #raise "Can not yet modify networks on a template"
-  end
-  def nic_model=(value)
-      raise "Can not modify network model on a template"
-  end
-  def graphics_type=(value)
-      raise "Can not modify graphics type on a template"
-  end
-  def graphics_listen=(value)
-      raise "Can not yet modify graphics listen port on a template"
-  end
-  def graphics_port=(value)
-      raise "Can not modify graphics_port on a template"
-  end
-  def grahics_passwd=(value)
-      raise "Can not yet modify graphics password on a template"
-  end
-  def graphics_keymap=(value)
-      raise "Can not yet modify graphics keymap on a template"
-  end
-  def context=(value)
-      #raise "Can not yet modify context hashes on a template"
-  end
-  def context_ssh=(value)
-      raise "Can not yet modify ssh context on a template"
-  end
-  def context_ssh_pubkey=(value)
-      raise "Can not yet modify root ssh pub key context on a template"
-  end
-  def context_network=(value)
-      raise "Can not yet modify network context on a template"
-  end
-  def context_onegate=(value)
-      raise "Can not modify onegate context on a template"
-  end
-  def context_files=(value)
-      #raise "Can not yet modify context files on a template"
-  end
-  def context_variables=(value)
-      raise "Can not yet modify context variables on a template"
-  end
-  def context_placement_host=(value)
-      raise "Can not yet modify host placement context on a template"
-  end
-  def context_placement_cluster=(value)
-      raise "Can not yet modify cluster placement context on a template"
-  end
-  def context_policy=(value)
-      raise "Can not yet modify placement policy context on a template"
   end
 end
--- spec/provider/onetemplate_spec.rb
+++ spec/provider/onetemplate_spec.rb
@@ -1,20 +1,92 @@
-#!/usr/bin/env rspec
-
+#!/usr/bin/env ruby
 require 'spec_helper'
 
 provider_class = Puppet::Type.type(:onetemplate).provider(:onetemplate)
+
 describe provider_class do
   let(:resource ) {
     Puppet::Type::Onetemplate.new({
-      :name => 'new_cluster',
+      :name    => 'app',
+      :disks   => [:one, :two],
+      :nics    => [:one, :two],
+      :memory  => '256',
+      :cpu     => '2',
     })
   }
 
+  let(:response) {{
+    :show => File.read('spec/fixtures/opennebula/xml/onetemplate_show.xml'),
+    :list => File.read('spec/fixtures/opennebula/xml/onetemplate_list.xml'),
+    :create => File.read('spec/fixtures/opennebula/xml/onetemplate_create.xml'),
+  }}
+
   let(:provider) {
-    @provider = provider_class.new(@resource)
+    provider = provider_class.new(:name => 'app')
+    provider.resource = resource
+    provider
   }
 
-  it 'should exist' do
-    @provider
+  context 'dynamic property bindings' do
+    before(:each) do
+      provider.stubs(:invoke).returns(response[:show]).once
+    end
+
+    it 'should have read access to memory' do
+      provider.memory.should eq ["128"]
+      expect { provider.memory = "1024" }.to raise_error(RuntimeError, /Can not yet modify memory on a onetemplate./)
+    end
+
+    it 'should have read access to cpu' do
+      provider.cpu.should eq ["0.1"]
+      expect { provider.cpu = "8" }.to raise_error(RuntimeError, /Can not yet modify cpu on a onetemplate./)
+    end
+  end
+
+  context 'template handler' do
+    it 'should create a template' do
+
+      temp = Tempfile.new('my-testrun')
+      Tempfile.stubs(:new).returns(temp)
+      temp.stubs(:write).with(response[:create])
+
+      provider.stubs(:invoke).with('create', temp.path).once
+      provider.create
+    end
+
+    it "should list templates" do
+      provider_class.stubs(:invoke).with('list', '--xml').returns(response[:list]).once
+      provider_class.stubs(:invoke).with('show', 'app', '--xml').returns(response[:show]).times(5)
+
+      template = provider_class.instances.first
+
+      provider.resource = template
+
+      provider.name.should eq "app"
+      provider.memory.should eq ["128"]
+      provider.cpu.should eq ["0.1"]
+      provider.vcpu.should eq ["2"]
+      provider.nic_model.should eq ["virtio", "virtio"]
+      provider.os_boot.should eq ["hd"]
+    end
+
+    it "should list existing templates" do
+      provider_class.stubs(:invoke).
+        with('list', '--xml').returns(response[:list]).times(4)
+
+      provider_class.instances[0].name.should eq 'app'
+      provider_class.instances[1].name.should eq 'database'
+
+      provider_class.instances[0].exists?.should be_truthy
+      provider_class.instances[1].exists?.should be_truthy
+
+      provider_class.stubs(:invoke).
+        with('list', '--xml').returns('<VMTEMPLATE_POOL></VMTEMPLATE_POOL>').once
+      provider_class.instances.should eq []
+    end
+
+    it "should delete a template" do
+      provider_class.stubs(:invoke).with('delete', 'app').once
+      provider.destroy
+    end
   end
 end
